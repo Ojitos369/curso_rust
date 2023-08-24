b937df796c2539bd86a0a1921aecbdfd18dff65d
ojitos369 <ojitos369@gmail.com>
2023-08-24 11:50:15 -0600

    variables e impresion

--- src/main.rs
+++ src/main.rs
@@ -7,6 +7,7 @@ fn temperatura() {
     let temperatura_maxima: u8 = 32;
     let temperatura_promedio: f32 = (temperatura_minima + temperatura_maxima as i8) as f32/ 2.0;
 
+    // print!("La termeratura minima en mi ciudad es {} y la maxima es {} y el promedio es {}", temperatura_minima, temperatura_maxima, temperatura_promedio);
     print!("La termeratura minima en mi ciudad es {temperatura_minima} y la maxima es {temperatura_maxima} y el promedio es {temperatura_promedio}");
 }
 
